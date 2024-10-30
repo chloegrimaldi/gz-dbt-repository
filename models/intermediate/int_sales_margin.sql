4197a0ee7adb4d3e39b13bdf79869368d8bf3368
Chloe <chloegrimaldi@hotmail.fr>
2024-10-30 15:48:09 +0000

    fde

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,7 +5,8 @@ WITH F AS (SELECT
     products_id,
     revenue,
     product.purchase_price,
-    quantity*purchase_price AS purchase_cost
+    quantity*purchase_price AS purchase_cost,
+ {{ margin_percent('sales.revenue', 'sales.quantity * CAST(product.purchase_price AS FLOAT64)') }}
 FROM {{ ref('stg_raw__sales') }} AS sales
 INNER JOIN {{ ref('stg_raw__products') }} AS product
 USING (products_id)
